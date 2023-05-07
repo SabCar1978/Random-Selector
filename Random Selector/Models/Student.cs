a8e92022a3e3887b20b298ea72290d7ed902eb7c
SabCar1978 <carette.saby@skynet.be>
2023-05-07 19:45:59 +0200

    ok

--- Random Selector/Models/Student.cs
+++ Random Selector/Models/Student.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Random_Selector.Models
+﻿namespace Random_Selector.Models
 {
     public class Student
     {
